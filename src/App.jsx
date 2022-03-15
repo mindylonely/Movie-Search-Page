58e64c2d7073a0570515c1f0a8fef1cb28001486
mindylonely <milulou2057@gmail.com>
2022-03-16 04:08:16 +0700

    edit 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
-import { apiRequest, searchRequest } from "./requestMethods";
+import { searchRequest } from "./requestMethods";
 
 const App = () => {
       const [searchTerm, setSearchTerm] = useState("");
